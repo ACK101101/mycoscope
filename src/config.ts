42581ea3c4d8f3c3d0125d233d28fed9c229ba07
ACK101101 <alexanderckumar@gmail.com>
2026-09-21 21:32:07 -0400

    setup

---
+++ src/config.ts
@@ -0,0 +1,9 @@
+interface Config<Names extends string> {
+  systems: { name: Names; path: string }[];
+  connections: [Names, Names][];
+  exempt: string[];
+}
+
+export function defineConfig<Names extends string>(config: Config<Names>): Config<Names> {
+  return config;
+}
